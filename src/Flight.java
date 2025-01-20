9987daec565d4655b0091c32c95e09777b9e7713
Emmanuel Cobbinah <emmanuelcobbinah707@gmail.com>
2025-01-20 11:48:27 +0000

    added a sub repo

--- src/Flight.java
+++ src/Flight.java
@@ -1,4 +1,83 @@
-package PACKAGE_NAME;
-
 public class Flight {
+    private String flightNumber;
+    private String destination;
+    private int capacity;
+    private int bookedSeats;
+
+    // Constructor
+    public Flight(String flightNumber, String destination, int capacity, int bookedSeats) {
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+        this.capacity = capacity;
+        this.bookedSeats = bookedSeats;
+    }
+
+    // Getters and Setters
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public void setFlightNumber(String flightNumber) {
+        this.flightNumber = flightNumber;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+
+        if (capacity < bookedSeats) {
+            System.out.println("Invalid capacity: It must be greater than or equal to booked seats");
+        } else {
+            this.capacity = capacity;
+        }
+
+    }
+
+    public int getBookedSeats() {
+        return bookedSeats;
+    }
+
+    public void setBookedSeats(int bookedSeats) {
+        this.bookedSeats = bookedSeats;
+    }
+
+
+    public void bookSeat() {
+        if (bookedSeats < capacity) {
+            bookedSeats++;
+
+            int remainingSeats = getCapacity() - getBookedSeats();
+
+            System.out.println("Seats booked successfully. Remaining seats: " + remainingSeats);
+        } else {
+            System.out.println("No seats available");
+        }
+
+    };
+
+    public void cancelSeat () {
+        if (bookedSeats > 0) {
+            bookedSeats--;
+            System.out.println("Seat cancellation successful. Available seats: " + bookedSeats);
+        } else {
+            System.out.println("No bookings to cancel.");
+        }
+    };
+
+
+
+
+
+
 }
